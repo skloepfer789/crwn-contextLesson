93641547cf817b1d84f081a95d3bf995f5aef37f
Steve <skloepfer789@yahoo.com>
2021-08-26 22:54:17 -0600

    finished the ContextAPI lecture. Fully functioning without Redux, though no persist

---
+++ src/providers/cart/cart.provider.jsx
@@ -0,0 +1,57 @@
+import React, {createContext, useState, useEffect} from 'react';
+
+import { 
+    addItemToCart, 
+    removeItemFromCart, 
+    filterItemFromCart,
+    getCartItemsCount,
+    getCartTotal
+} from './cart.utils';
+
+
+export const CartContext = createContext({
+    hidden: true,
+    toggleHidden: () => {},
+    cartItems: [],
+    addItem: () => {},
+    removeItem: () => {},
+    clearItemFromCart: () => {},
+    cartItemsCount: 0,
+    cartTotal: 0
+});
+
+const CartProvider = ({children}) => {
+    const [hidden, setHidden] = useState(true);
+    const [cartItems, setCartItems] = useState([]);
+    const [cartItemsCount, setCartItemsCount] = useState(0);
+    const [cartTotal, setCartTotal] = useState(0);
+
+    const addItem = item => setCartItems(addItemToCart(cartItems, item));
+    const removeItem = item => setCartItems(removeItemFromCart(cartItems, item));
+    const clearItemFromCart = item => setCartItems(filterItemFromCart(cartItems, item));
+
+    useEffect(() => {
+        setCartItemsCount(getCartItemsCount(cartItems))
+    }, [cartItems]);
+
+    useEffect(() => {
+        setCartTotal(getCartTotal(cartItems))
+    }, [cartItems]);
+
+    const toggleHidden = () => setHidden(!hidden);
+    
+    return <CartContext.Provider
+        value={{
+            hidden,
+            toggleHidden,
+            cartItems,
+            addItem,
+            cartItemsCount,
+            removeItem,
+            clearItemFromCart,
+            cartTotal
+        }}
+    >{children}</CartContext.Provider>;
+};
+
+export default CartProvider;
